1c2397a07fd3e98e87c2f038b9e99ddec00a3f17
clevelandjosh <33519616+clevelandjosh@users.noreply.github.com>
2019-01-31 02:25:22 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if [ ! -e ~/CentOS7 ] 
+if [ ! -e ./CentOS7 ] 
 then
-mkdir ~/CentOS7
-cd ~/CentOS7
+mkdir ./CentOS7
+cd ./CentOS7
 else 
 echo "Can't create the CentOS7 directory, as one is already in place"
 exit
@@ -11,5 +11,4 @@ fi
 pwd
 
 vagrant box add --provider=virtualbox centos/7
-vagrant init centos/7
 vagrant up
